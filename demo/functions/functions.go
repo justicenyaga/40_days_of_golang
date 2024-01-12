5dfd434c5be8d79c296b7e94211a18fc52a43efc
Justice Nyaga <justiceevansmwema@gmail.com>
2024-01-12 14:03:21 +0300

    Working with functions

--- demo/functions/functions.go
+++ demo/functions/functions.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
+func double(x int) int {
+	return x + x
+}
 
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func greet() {
+	fmt.Println("Hello from the greet function")
+}
+
+func main() {
+	greet()
+
+	dozen := double(6)
+	fmt.Println("A dozen has", dozen, "items")
+
+	backersDozen := add(dozen, 1)
+	fmt.Println("A backersDozen has", backersDozen, "items")
 }
