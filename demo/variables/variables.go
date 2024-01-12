c9cdef5736e2c1557ffda86de9124470568b3c01
Justice Nyaga <justiceevansmwema@gmail.com>
2024-01-12 14:02:08 +0300

    Working with go variables

--- demo/variables/variables.go
+++ demo/variables/variables.go
@@ -3,4 +3,34 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Justice Nyaga"
+	fmt.Println("My name is", myName)
+
+	var name string = "Amos"
+	fmt.Println("name =", name)
+
+	username := "admin"
+	fmt.Println("username =", username)
+
+	var sum int
+	fmt.Println("The sum is =", sum)
+
+	part1, other := 1, 5
+	fmt.Println("part 1 is", part1, "and other is", other)
+
+	part2, other := 2, 0
+	fmt.Println("part 2 is", part2, "and other is", other)
+
+	sum = part1 + part2
+	fmt.Println("Sum =", sum)
+
+	var (
+		lessonName = "Variable"
+		lessonType = "Demo"
+	)
+	fmt.Println("lessonName =", lessonName)
+	fmt.Println("lessonType =", lessonType)
+
+	word1, word2, _ := "hello", "world", "!"
+	fmt.Println(word1, word2)
 }
--- exercise/variables/variables.go
+++ exercise/variables/variables.go
@@ -4,13 +4,6 @@
 //  formatting you like.
 //
 //Requirements:
-//* Store your favorite color in a variable using the `var` keyword
-//* Store your birth year and age (in years) in two variables using
-//  compound assignment
-//* Store your first & last initials in two variables using block assignment
-//* Declare (but don't assign!) a variable for your age (in days),
-//  then assign it on the next line by multiplying 365 with the age
-// 	variable created earlier
 //
 //Notes:
 //* Use fmt.Println() to print out information
@@ -25,4 +18,24 @@ package main
 import "fmt"
 
 func main() {
+	//* Store your favorite color in a variable using the `var` keyword
+	var color = "blue"
+	//* Store your birth year and age (in years) in two variables using
+	//  compound assignment
+	birthYear, age := 2002, 21
+	//* Store your first & last initials in two variables using block assignment
+	var (
+		firstInitial = "J"
+		lastInitial  = "N"
+	)
+	//* Declare (but don't assign!) a variable for your age (in days),
+	var ageInDays int
+	//  then assign it on the next line by multiplying 365 with the age
+	// 	variable created earlier
+	ageInDays = 365 * age
+
+	fmt.Println("My favorite color is:", color)
+	fmt.Println("I was born in", birthYear, "and I'm ", age, "years old.")
+	fmt.Println("My first initial is", firstInitial, "and last initial is", lastInitial)
+	fmt.Println("I am", ageInDays, "days old")
 }
