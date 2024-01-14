ddb761bb041e6faa2d43937d56a29662bce45a5b
Justice Nyaga <justiceevansmwema@gmail.com>
2024-01-14 18:31:17 +0300

    Working with interfaces

--- exercise/interfaces/interfaces.go
+++ exercise/interfaces/interfaces.go
@@ -7,7 +7,7 @@
 //  - Motorcycles: small lifts
 //  - Cars: standard lifts
 //  - Trucks: large lifts
-//* Write a single function to handle all of the vehicles
+//* Write a single function to handle all the vehicles
 //  that the shop works on.
 //* Vehicles have a model name in addition to the vehicle type:
 //  - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
@@ -21,6 +21,63 @@ package main
 
 import "fmt"
 
-func main() {
+const (
+	SmallLift = iota
+	StandardLift
+	LargeLift
+)
 
+type Lift int
+
+type LiftPicker interface {
+	PickLift() Lift
+}
+
+type Motorcycle string
+type Car string
+type Truck string
+
+func (m Motorcycle) String() string {
+	return fmt.Sprintf("Motorcycle: %v", string(m))
+}
+
+func (c Car) String() string {
+	return fmt.Sprintf("Car: %v", string(c))
+}
+
+func (t Truck) String() string {
+	return fmt.Sprintf("Truck: %v", string(t))
+}
+
+func (m Motorcycle) PickLift() Lift {
+	return SmallLift
+}
+
+func (c Car) PickLift() Lift {
+	return StandardLift
+}
+
+func (t Truck) PickLift() Lift {
+	return LargeLift
+}
+
+func sendToLift(p LiftPicker) {
+	switch p.PickLift() {
+	case SmallLift:
+		fmt.Printf("Send %v to small lift\n", p)
+	case StandardLift:
+		fmt.Printf("Send %v to standard lift\n", p)
+	case LargeLift:
+		fmt.Printf("Send %v to large lift\n", p)
+	}
+}
+
+func main() {
+	car := Car("Sporty")
+	motorcycle := Motorcycle("Kawasaki")
+	truck := Truck("Mountain Devourer")
+
+	sendToLift(car)
+	sendToLift(motorcycle)
+	sendToLift(truck)
 }
