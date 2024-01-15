b4720ef40da5f54531b7fba1c15f0f7568d200f1
Justice Nyaga <justiceevansmwema@gmail.com>
2024-01-15 13:21:14 +0300

    Handling errors

--- demo/errors/errors.go
+++ demo/errors/errors.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func main() {
+type DivError string
 
+func (de DivError) Error() string {
+	return fmt.Sprintf("error dividing: %v", string(de))
+}
+
+func divide(lhs, rhs int) (int, error) {
+	if rhs == 0 {
+		divError := DivError("ZeroDivError")
+		return 0, divError
+	}
+	return lhs / rhs, nil
+}
+
+func main() {
+	lhs, rhs := 10, 0
+	res, err := divide(lhs, rhs)
+	if err != nil {
+		zeroDivErr := DivError("ZeroDivError")
+		if errors.Is(err, zeroDivErr) {
+			fmt.Println("Division by Zero Error:", err)
+		} else {
+			fmt.Println("OtherError:", err)
+		}
+	} else {
+		fmt.Println(res)
+	}
 }
